f5dee08973f21ef1ab9ef6852515894193d6d3cf
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2009-07-07 22:56:24 -0300

    changed jquery for extjs, because of a better layout and design capabilities

--- source/event-manager-web/src/main/webapp/js/layout.js
+++ source/event-manager-web/src/main/webapp/js/layout.js
@@ -1,32 +1,117 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+Ext.onReady(function(){
 
-$(function(){
-    layout = null;
-    settings = {
-        defaults: {
-            applyDefaultStyles:				true, // basic styling for testing & demo purposes
-            south_size: 50,
-            center_size: 50
-        },
-        west:{
-          size:300,
-          maxSize: 300
-        },
-        center: {
-            width: 300,
-            maxWidht:300,
-            resizable: false
-        },
-        east:{
-            size:500,
-            minWidth:500
-        }
+    // NOTE: This is an example showing simple state management. During development,
+    // it is generally best to disable state management as dynamically-generated ids
+    // can change across page loads, leading to unpredictable results.  The developer
+    // should ensure that stable state ids are set for stateful components in real apps.
+    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
 
-    }
-    layout = $('body').layout(settings);
+    var viewport = new Ext.Viewport({
+        layout: 'border',
+        items: [
+        // create instance immediately
+        new Ext.BoxComponent({
+            region: 'north',
+            height: 32, // give north and south regions a height
+            autoEl: {
+                tag: 'div',
+                html:'<p>north - generally for menus, toolbars and/or advertisements</p>'
+            }
+        }), {
+            // lazily created panel (xtype:'panel' is default)
+            region: 'south',
+            contentEl: 'south',
+            split: true,
+            height: 100,
+            minSize: 100,
+            maxSize: 200,
+            collapsible: true,
+            title: 'South',
+            margins: '0 0 0 0'
+        }, {
+            region: 'east',
+            title: 'East Side',
+            collapsible: true,
+            split: true,
+            width: 225, // give east and west regions a width
+            minSize: 175,
+            maxSize: 400,
+            margins: '0 5 0 0',
+            layout: 'fit', // specify layout manager for items
+            items:            // this TabPanel is wrapped by another Panel so the title will be applied
+            new Ext.TabPanel({
+                border: false, // already wrapped so don't add another border
+                activeTab: 1, // second tab initially active
+                tabPosition: 'bottom',
+                items: [{
+                    html: '<p>A TabPanel component can be a region.</p>',
+                    title: 'A Tab',
+                    autoScroll: true
+                }, new Ext.grid.PropertyGrid({
+                    title: 'Property Grid',
+                    closable: true,
+                    source: {
+                        "(name)": "Properties Grid",
+                        "grouping": false,
+                        "autoFitColumns": true,
+                        "productionQuality": false,
+                        "created": new Date(Date.parse('10/15/2006')),
+                        "tested": false,
+                        "version": 0.01,
+                        "borderWidth": 1
+                    }
+                })]
+            })
+        }, {
+            region: 'west',
+            id: 'west-panel', // see Ext.getCmp() below
+            title: 'West',
+            split: true,
+            width: 200,
+            minSize: 175,
+            maxSize: 400,
+            collapsible: true,
+            margins: '0 0 0 5',
+            layout: {
+                type: 'accordion',
+                animate: true
+            },
+            items: [{
+                contentEl: 'west',
+                title: 'Navigation',
+                border: false,
+                iconCls: 'nav' // see the HEAD section for style used
+            }, {
+                title: 'Settings',
+                html: '<p>Some settings in here.</p>',
+                border: false,
+                iconCls: 'settings'
+            }]
+        },
+        // in this instance the TabPanel is not wrapped by another panel
+        // since no title is needed, this Panel is added directly
+        // as a Container
+        new Ext.TabPanel({
+            region: 'center', // a center region is ALWAYS required for border layout
+            deferredRender: false,
+            activeTab: 0,     // first tab initially active
+            items: [{
+                contentEl: 'center1',
+                title: 'Close Me',
+                closable: true,
+                autoScroll: true
+            }, {
+                contentEl: 'center2',
+                title: 'Center Panel',
+                autoScroll: true
+            }]
+        })]
+    });
+    // get a reference to the HTML element with id "hideit" and add a click listener to it
+    Ext.get("hideit").on('click', function(){
+        // get a reference to the Panel that was created with id = 'west-panel'
+        var w = Ext.getCmp('west-panel');
+        // expand or collapse that Panel based on its collapsed property state
+        w.collapsed ? w.expand() : w.collapse();
+    });
 });
-
-
