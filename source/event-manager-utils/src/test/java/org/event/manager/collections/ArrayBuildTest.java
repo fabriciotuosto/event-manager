7f6092c7c44bbf0cc31be493bd7ccb4ad8342139
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2009-07-14 13:32:39 -0300

    Removed bad code in test case that was making right code to fail test

--- source/event-manager-utils/src/test/java/org/event/manager/collections/ArrayBuildTest.java
+++ source/event-manager-utils/src/test/java/org/event/manager/collections/ArrayBuildTest.java
@@ -1,7 +1,8 @@
 package org.event.manager.collections;
 
+import static org.testng.Assert.assertNotNull;
+
 import java.util.Arrays;
-import static org.testng.Assert.*;
 
 import org.event.manager.test.TestGroup;
 import org.testng.annotations.DataProvider;
@@ -18,16 +19,15 @@ public class ArrayBuildTest {
 	@DataProvider(name = "array")
 	public Object[][] dataPriver() {
 		return new Object[][] {
-                    new Object[] {new Object[]{ "1", "2", "3" }},
-		    new Object[] {}, new Object[] {new Object[]{ null, "not null", }},
-                    new Object[] {}, new Object[] {new Object[]{ null, 1L,"1" }}
-                };
+				new Object[] { new Object[] { "1", "2", "3" } },
+				new Object[] { new Object[] { null, "not null", } },
+				new Object[] { new Object[] { null, 1L, "1" } } };
 	}
 
 	@Test(dataProvider = "array")
 	public <T> void test_creation_with_different_data(T... data) {
-            System.out.println(Arrays.toString(data));
-            assertNotNull(ArrayBuild.of(data));
+		System.out.println(Arrays.toString(data));
+		assertNotNull(ArrayBuild.of(data));
 	}
 
 }
