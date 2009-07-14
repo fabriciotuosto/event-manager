7122e46bc80d05c8dfa6e79c7692e70730a4711b
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2009-07-14 13:06:59 -0300

    added test for ArraBuild

--- source/event-manager-utils/src/test/java/org/event/manager/collections/ArrayBuildTest.java
+++ source/event-manager-utils/src/test/java/org/event/manager/collections/ArrayBuildTest.java
@@ -1,8 +1,10 @@
 package org.event.manager.collections;
 
+import java.util.Arrays;
 import static org.testng.Assert.assertNotNull;
 
 import org.event.manager.test.TestGroup;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test(groups = { TestGroup.UNIT })
@@ -12,4 +14,20 @@ public class ArrayBuildTest {
 	public void create_simple_array() {
 		assertNotNull(ArrayBuild.of());
 	}
+
+	@DataProvider(name = "array")
+	public Object[][] dataPriver() {
+		return new Object[][] {
+                    new Object[] {new Object[]{ "1", "2", "3" }},
+		    new Object[] {}, new Object[] {new Object[]{ null, "not null", }},
+                    new Object[] {}, new Object[] {new Object[]{ null, 1L,"1" }}
+                };
+	}
+
+	@Test(dataProvider = "array")
+	public <T> void test_creation_with_different_data(T... data) {
+            System.out.println(Arrays.toString(data));
+            assertNotNull(ArrayBuild.of(data));
+	}
+
 }
