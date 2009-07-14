22e89784fd6074533e91b51df1c18d2d8189a766
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2009-07-14 12:12:32 -0300

    corrected maven-surefire-plugin that was incorrectly configured in maven-surefire-report-plugin by mistake added a class that holds constants for testng groups

---
+++ source/event-manager-utils/src/main/java/org/event/manager/test/TestGroup.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.event.manager.test;
+
+/**
+ * 
+ * @author tuosto
+ */
+public final class TestGroup {
+
+	public static final String UNIT = "unit";
+	public static final String INTEGRATION = "integration";
+	public static final String REGRESSION = "regresion";
+
+	private TestGroup() {
+	}
+}
--- source/event-manager-utils/src/test/java/org/event/manager/collections/ArrayBuildTest.java
+++ source/event-manager-utils/src/test/java/org/event/manager/collections/ArrayBuildTest.java
@@ -2,9 +2,10 @@ package org.event.manager.collections;
 
 import static org.testng.Assert.assertNotNull;
 
+import org.event.manager.test.TestGroup;
 import org.testng.annotations.Test;
 
-@Test(groups = { "unit" })
+@Test(groups = { TestGroup.UNIT })
 public class ArrayBuildTest {
 
 	@Test
