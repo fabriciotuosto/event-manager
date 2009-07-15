56f09a77d45840bb0791a38ded51ad602b21861c
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2009-07-15 13:30:49 -0300

    Added user builder, to create user, and added minimal behavior

--- source/event-manager-api/src/main/java/org/event/manager/entities/User.java
+++ source/event-manager-api/src/main/java/org/event/manager/entities/User.java
@@ -1,8 +1,8 @@
 package org.event.manager.entities;
 
 import java.io.Serializable;
-
 import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,9 +14,10 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.Validate;
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.event.manager.Builder;
+import org.event.manager.utils.EmailUtils;
 
 /**
  * 
@@ -34,18 +35,53 @@ public class User implements Serializable {
 	private Long id;
 	private String name;
 	private String email;
-        private String password;
-        private Set<Group> groups;
-        private Set<User> contacts;
+	private String password;
+	private Set<Group> groups;
+	private Set<User> contacts;
 
-	public String getEmail() {
-		return email;
+	/**
+	 * 
+	 * @param name
+	 * @param email
+	 * @param password
+	 * @return
+	 */
+	public static UserBuilder createUser(String name, String email,
+			String password) {
+		return new UserBuilder(name, email, password);
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	/**
+	 * Default constructor for JavaBeans Specification needed for frameworks
+	 */
+	public User() {
+		super();
 	}
 
+	/**
+	 * Easy creates a user with some id in order to provide an easy example to
+	 * search in jpa
+	 * 
+	 * @param id
+	 */
+	public User(Long id) {
+		this();
+		this.id = id;
+	}
+
+	/**
+	 * Build a new user from the user builder
+	 * 
+	 * @param builder
+	 */
+	private User(UserBuilder builder) {
+		id = builder.id;
+		name = builder.name;
+		email = builder.email;
+		password = builder.password;
+		groups = builder.groups;
+		contacts = builder.contacts;
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -53,33 +89,29 @@ public class User implements Serializable {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public User setId(Long id) {
 		this.id = id;
+		return this;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public User setEmail(String email) {
+		this.email = email;
+		return this;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	public User setName(String name) {
 		this.name = name;
+		return this;
 	}
-
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj);
-	}
-
-	@Override
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
-	}
-
+	
 	/**
 	 * @return the password
 	 */
@@ -91,37 +123,115 @@ public class User implements Serializable {
 	 * @param password
 	 *            the password to set
 	 */
-	public void setPassword(String password) {
+	public User setPassword(String password) {
 		this.password = password;
+		return this;
 	}
 
-    /**
-     * @return the groups
-     */
-    @OneToMany(targetEntity=Group.class)
-    public Set<Group> getGroups() {
-        return groups;
-    }
+	/**
+	 * @return the groups
+	 */
+	@OneToMany
+	public Set<Group> getGroups() {
+		return groups;
+	}
 
-    /**
-     * @param groups the groups to set
-     */
-    public void setGroups(Set<Group> groups) {
-        this.groups = groups;
-    }
+	/**
+	 * @param groups
+	 *            the groups to set
+	 */
+	public User setGroups(Set<Group> groups) {
+		this.groups = groups;
+		return this;
+	}
+	
+	/**
+	 * 
+	 * @param groups
+	 * @return
+	 */
+	public User addGroup(Group... groups){
+		for(Group group: groups){
+			this.groups.add(group);
+		}
+		return this;
+	}
+	
+	/**
+	 * @return the contacts
+	 */
+	@OneToMany
+	public Set<User> getContacts() {
+		return contacts;
+	}
+	
+	/**
+	 * 
+	 * @param contacs
+	 * @return
+	 */
+	public User addContact(User... contacs){
+		for(User contact: contacs){
+			this.contacts.add(contact);
+		}
+		return this;
+	}
+	/**
+	 * @param contacts
+	 *            the contacts to set
+	 */
+	public void setContacts(Set<User> contacts) {
+		this.contacts = contacts;
+	}	
 
-    /**
-     * @return the contacts
-     */
-    @OneToMany(targetEntity=User.class)
-    public Set<User> getContacts() {
-        return contacts;
-    }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
-    /**
-     * @param contacts the contacts to set
-     */
-    public void setContacts(Set<User> contacts) {
-        this.contacts = contacts;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		boolean retval = false;
+		if (obj instanceof User) {
+			User other = (User) obj;
+			if (this.id != null) {
+				retval = this.id.equals(other.id);
+			}
+		}
+		return retval;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+
+
+	public static class UserBuilder implements Builder<User> {
+
+		private Long id;
+		private String name;
+		private String email;
+		private String password;
+		private Set<Group> groups;
+		private Set<User> contacts;
+
+		private UserBuilder(String name, String email, String password) {
+			Validate.notNull(name, "The name of the user cannot be null");
+			Validate.notNull(password,
+					"The password of the user cannot be null");
+			Validate.notNull(email, "The email of the user cannot be null");
+			Validate.isTrue(EmailUtils.isEmailAdressValid(email),
+					"The email must be a valid mail address");
+			this.name = name;
+			this.email = email;
+			this.password = password;
+		}
+
+		@Override
+		public User build() {
+			return new User(this);
+		}
+	}
 }
---
+++ source/event-manager-utils/src/main/java/org/event/manager/Builder.java
@@ -0,0 +1,16 @@
+package org.event.manager;
+
+/**
+ * 
+ * @author tuosto
+ * 
+ * @param <T>
+ */
+public interface Builder<T> {
+
+	/**
+	 * 
+	 * @return
+	 */
+	T build();
+}
