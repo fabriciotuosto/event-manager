346dc6c24401dfd546bed92d8430488724faaa4f
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2009-08-05 22:37:03 -0300

    removed unnesesary entity to be persisted

--- source/event-manager-domain/src/main/java/org/event/manager/entities/Response.java
+++ source/event-manager-domain/src/main/java/org/event/manager/entities/Response.java
@@ -1,25 +1,11 @@
 package org.event.manager.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@Entity
 @XmlRootElement
 public enum Response {
-	YES(1L),
-	NO(2L),
-	MAYBE(3L);
-	
-	Response(Long id){
-		this.id = id;
-	}
-	
-	private final Long id;
-
-	@Id
-	public Long getId() {
-		return id;
-	}
+	YES,
+	NO,
+	MAYBE;
 
 }
